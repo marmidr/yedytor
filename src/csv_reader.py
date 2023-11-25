84c33f86738de01ee07fdb9afea25e68612ed4bc
Mariusz Midor <mmidor@gmail.com>
2023-11-25 13:05:41 +0100

    CSV reader: detect if ' is used as a quote char instead of "

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -34,6 +34,34 @@ def __read_sp(rows: list[str], tg: TextGrid):
             tg.rows_raw().append(row_cells_processed)
     return max_cols
 
+def __read_csv(file, tg: TextGrid, delim: str, quote_char: str = '"'):
+    max_cols = 0
+    reader = csv.reader(file, delimiter=delim, quotechar=quote_char)
+    for row in reader:
+        # ignore rows with empty cell 'A' or cell with a long horizontal line
+        if len(row) > 0 and row[0] != "" and not row[0].startswith("___"):
+            row_cells = [cell.strip() for cell in row]
+            max_cols = max(max_cols, len(row_cells))
+            tg.rows_raw().append(row_cells)
+
+    # check if cell starts and ends with the apostrophe
+    apostr_as_quotechar = False
+    if quote_char != "'" and max_cols > 1 and len(tg.rows_raw()) > 2:
+        for (c, r1_cell) in enumerate(tg.rows_raw()[1]):
+            if r1_cell.startswith("'") and r1_cell.endswith("'"):
+                r2_cell = tg.rows_raw()[2][c]
+                if r2_cell.startswith("'") and r2_cell.endswith("'"):
+                    apostr_as_quotechar = True
+                    break
+
+    if apostr_as_quotechar:
+        tg.rows_raw().clear()
+        file.seek(0)
+        logging.debug("  Reload CSV with ' as a quotechar")
+        return __read_csv(file, tg, delim, "'")
+
+    return max_cols
+
 def read_csv(path: str, delim: str) -> TextGrid:
     """
     Reads entire CSV/text file.
@@ -58,13 +86,7 @@ def read_csv(path: str, delim: str) -> TextGrid:
             # TODO: add reader for reg-ex
             raise ValueError("delimiter *re not yet implemented")
         else:
-            reader = csv.reader(f, delimiter=delim)
-            for row in reader:
-                # ignore rows with empty cell 'A' or cell with a long horizontal line
-                if len(row) > 0 and row[0] != "" and not row[0].startswith("___"):
-                    row_cells = [cell.strip() for cell in row]
-                    max_cols = max(max_cols, len(row_cells))
-                    tg.rows_raw().append(row_cells)
+            max_cols = __read_csv(f, tg, delim)
 
     tg.nrows = len(tg.rows_raw())
     tg.ncols = max_cols
